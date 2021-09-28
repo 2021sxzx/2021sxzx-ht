3b90224a94cb1c53db1b5ec56cbdd71ebe770e92
xxih <2217911664@qq.com>
2021-09-28 17:40:23 +0800

    feat: 完成评价报告页面，缺少搜索功能。

--- src/views/page-container/comment-manage/CommentManageReport.js
+++ src/views/page-container/comment-manage/CommentManageReport.js
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import { Input, Space } from 'antd';
+import style from './CommentManageReport.module.css'
+import SearchBar from './components/SearchBar.js'
+import Charts from './components/Charts.js'
+const { Search } = Input;
 export default function CommentManageReport() {
+
   return (
     <div>
-      CommentManageReport
+      <SearchBar></SearchBar>
+      <Charts></Charts>
     </div>
   )
 }
