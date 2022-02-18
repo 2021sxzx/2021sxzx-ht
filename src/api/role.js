430b3911ceb4431ad567a8fe82274db020d54f0c
UmyNya <1079102275@qq.com>
2022-02-18 16:22:28 +0800

    feat: 实现了角色管理相关的 API，能够和服务器进行通信了。 但是服务端的角色搜索增加和修改这几个API可能有问题，好像不能处理一个角色有多个权限的情况

--- src/api/role.js
+++ src/api/role.js
@@ -8,14 +8,37 @@ import service from "./http";
 const api = {
     // 增加角色
     AddRole(data){
-        return 
+        return service.request({
+            method: "post",
+            url: "/v1/role/",
+            data, //data:data同名可以直接写 data
+        });
     },
 
-    // TODO(zzj): 可能还需要删除角色的 API
+    DeleteRole(data){
+        return service.request({
+            method: "delete",
+            url: "/v1/role/",
+            data, //data:data同名可以直接写 data
+        })
+    },
 
-    // 更新角色信息
-    updateRole(data){
-        return 
+    // 更新角色非权限相关的信息
+    UpdateRole(data){
+        return service.request({
+            method: "patch",
+            url: "/v1/role/",
+            data, //data:data同名可以直接写 data
+        })
+    },
+
+    // 修改用户权限(更新角色权限相关的信息)
+    UpdateRolePermission(data){
+        return service.request({
+            method: "patch",
+            url: "/v1/permission/",
+            data, //data:data同名可以直接写 data
+        })
     },
 
     // 获取角色数据
@@ -26,17 +49,15 @@ const api = {
             data, //data:data同名可以直接写 data
         });
     },
-
-    // 下面这个函数不知道要用来干什么
-    // async getCommentparams(){
-    //     return service.get('/v1/commentparam')
-    // },
+    async getRole(){
+        return service.get('/v1/role')
+    },
 
     // 搜索角色
     SearchRole(data) {
         return service.request({
             method: "post",
-            url: "/v1/searchRole/",
+            url: "/v1/searchRole",
             data, //data:data同名可以直接写 data
         });
     }
