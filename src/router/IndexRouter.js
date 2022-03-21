97914dd3bf41a171fe25d4dfa4caf701f1ac291c
BK201-Drama <1243532812@qq.com>
2022-03-21 17:28:41 +0800

    feat: 合并management

--- src/router/IndexRouter.js
+++ src/router/IndexRouter.js
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {HashRouter,Redirect,Route,Switch} from 'react-router-dom'
 import Login from '../views/login/Login'
-import PageContainer from '../views/PageContainer/PageContainer'
+import PageContainer from '../views/PageContainer/PageContainer';
 
 export default function indexRouter() {
+
+  const [isPermitived, setIsPermitived] = useState(true);
+  useEffect(() => {
+    // 获取用户权限字段，比如业务员
+    // 如果跟url不符合，就设置为false
+    setIsPermitived(false);
+  }, [])
+
   return (
     <HashRouter>
       <Switch>
         <Route path='/login' component={Login}></Route>
         <Route path='/' render={()=>
+          isPermitived ? <Login /> :
           <PageContainer></PageContainer>
         }>
         </Route>
