cfd0d6811ecdc2da6dea77650e5750363520fe8f
xxih <2217911664@qq.com>
2022-04-10 15:33:05 +0800

    refactor: 评价报告页面稍微重构，添加注释。

--- src/views/PageContainer/CommentManageReport/components/Charts.js
+++ src/views/PageContainer/CommentManageReport/components/Charts.js
@@ -7,31 +7,45 @@ export default function Charts() {
 	const chartsRef = useRef()
 	const [data, setData] = useState({})
 	useEffect(() => {
-		async function getComment() {
+		// 发送网络请求取得数据，进行处理，并存入 data
+		async function initChart() {
+			// 请求 rawData
 			let {
-				data: { data }
+				data: { data:rawData }
 			} = await api.getCommentParams()
-			let scoreInfo = [0, 0, 0, 0, 0]
-			data.scoreInfo?.forEach((item) => {
-				scoreInfo[parseInt(item.score) - 1] = item.count
-			})
-			let scoreInfoRemain = scoreInfo.map((item) => {
-				return data.totalNum - item
-			})
-			data.scoreInfo = scoreInfo
-			data.scoreInfoRemain = scoreInfoRemain
-			let pieInfo = [
-				{ name: '差', value: 0 },
-				{ name: '中', value: 0 },
-				{ name: '好', value: 0 }
-			]
-			pieInfo[0].value = scoreInfo[0]
-			pieInfo[1].value = scoreInfo[1]
-			for (let i = 2; i <= 4; i++) {
-				pieInfo[2].value += scoreInfo[i]
+			function processData(rawData) {
+				let processedData = JSON.parse(JSON.stringify(rawData))
+				// 下面三个数据，将通过加工原数据得到。附加在 processedData 上。
+				let scoreInfo = [0, 0, 0, 0, 0]
+				let scoreInfoRemain
+				let pieInfo
+
+				processedData.scoreInfo?.forEach((item) => {
+					scoreInfo[parseInt(item.score) - 1] = item.count
+				})
+				scoreInfoRemain = scoreInfo.map((item) => {
+					return processedData.totalNum - item
+				})
+				processedData.scoreInfo = scoreInfo
+				processedData.scoreInfoRemain = scoreInfoRemain
+				pieInfo = [
+					{ name: '差', value: 0 },
+					{ name: '中', value: 0 },
+					{ name: '好', value: 0 }
+				]
+				pieInfo[0].value = scoreInfo[0]
+				pieInfo[1].value = scoreInfo[1]
+				for (let i = 2; i <= 4; i++) {
+					pieInfo[2].value += scoreInfo[i]
+				}
+				processedData.pieInfo = pieInfo
+				processedData.goodRate = Math.round((pieInfo[2].value / processedData.totalNum) * 100)
+				// 下面是 echarts 的配置。
+				return processedData
 			}
-			data.pieInfo = pieInfo
-			data.goodRate = Math.round((pieInfo[2].value / data.totalNum) * 100)
+			// 处理数据
+			let processedData = processData(rawData)
+			// 用处理完成的数据构造 option
 			const option = {
 				color: ['#bae7ff', '#69c0ff', '#40a9ff'],
 				tooltip: {
@@ -52,7 +66,7 @@ export default function Charts() {
 				xAxis: {
 					show: false,
 					type: 'value',
-					max: data.totalNum
+					max: processedData.totalNum
 				},
 				yAxis: {
 					axisTick: { show: false },
@@ -84,7 +98,7 @@ export default function Charts() {
 						width: '40%',
 
 						top: 10,
-						data: data.pieInfo,
+						data: processedData.pieInfo,
 						colorBy: 'color',
 						emphasis: {
 							itemStyle: {
@@ -101,7 +115,7 @@ export default function Charts() {
 						}
 					},
 					{
-						data: data.scoreInfo,
+						data: processedData.scoreInfo,
 						stack: 'bar1',
 						type: 'bar',
 						barMaxWidth: 15,
@@ -110,7 +124,7 @@ export default function Charts() {
 						}
 					},
 					{
-						data: data.scoreInfoRemain,
+						data: processedData.scoreInfoRemain,
 						stack: 'bar1',
 						type: 'bar',
 						itemStyle: {
@@ -119,11 +133,13 @@ export default function Charts() {
 					}
 				]
 			}
-			setData(data)
+			// 除了 echart，界面也有需要这些数据的地方，所以要存储在状态中。
+			setData(processedData)
+			// 初始化 echarts
 			let myChart = echarts.init(chartsRef.current)
 			option && myChart.setOption(option)
 		}
-		getComment()
+		initChart()
 	}, [])
 
 	return (
