4a47c1b967fcd1f605fa189694d2cc79dd3fba27
张奕凯 <1781315832@qq.com>
2022-04-18 15:08:36 +0800

    修一下日志管理bug

--- src/views/PageContainer/SystemManage/SystemManageJournal/SystemManageJournal.js
+++ src/views/PageContainer/SystemManage/SystemManageJournal/SystemManageJournal.js
@@ -20,7 +20,8 @@ import api from "../../../../api/log";
 const SelectForm = (props) => {
   const [topForm] = Form.useForm();
   const [underForm] = Form.useForm();
-  const [myself, setMyself] = useState('false');
+  const [myself, setMyself] = useState(false);
+  const [myselfID, setMyselfID] = useState('');
   const [today, setToday] = useState(false);
   const [thisWeek, setThisWeek] = useState(false);
   const [groupVal, setGroupVal] = useState(0);
@@ -28,12 +29,12 @@ const SelectForm = (props) => {
   const handleToday=e=>{
     setToday(true);
     setThisWeek(false);
-    console.log("today:",today,".week:",thisWeek)
+    // console.log("today:",today,".week:",thisWeek)
   }
   const handleWeek=e=>{
     setToday(false);
     setThisWeek(true);
-    console.log("week:",thisWeek,".today:",today)
+    // console.log("week:",thisWeek,".today:",today)
   }
   const handleRadio=(e)=>{
     // console.log('value:',val)
@@ -41,7 +42,7 @@ const SelectForm = (props) => {
     //   setToday(false);
     //   setThisWeek(false);
     // }
-    console.log('radio checked',e.target.value);
+    // console.log('radio checked',e.target.value);
     setGroupVal(e.target.value);
     if(e.target.value===1){
       handleToday()
@@ -51,15 +52,13 @@ const SelectForm = (props) => {
     }
   }
   const Search = () => {
-    if (myself) {
-      console.log(localStorage.getItem('_id'))
-      setMyself(localStorage.getItem('_id'))
-    }
+    // console.log('::',myself,'--',myselfID)
     const data = {
-      myself,
+      myselfID,
       today,
       thisWeek,
     };
+    // console.log(data)
     props.getSearch(data);
   };
   const onReset = () => {
@@ -72,44 +71,66 @@ const SelectForm = (props) => {
     },
   };
   return (
-      <>
-        {/* <Form form={topForm} layout="inline">
+    <>
+      {/* <Form form={topForm} layout="inline">
           <Form.Item>
             <Button htmlType="button" onClick={onReset}>查询日志</Button>
           </Form.Item>
           <Form.Item name="InputSearch">
             <Input style={{width: 600}} /> */}
-            {/* <Input style={{ width: 600 }} enterButton="查询日志"></Input> */}
-          {/* </Form.Item>
+      {/* <Input style={{ width: 600 }} enterButton="查询日志"></Input> */}
+      {/* </Form.Item>
         </Form> */}
-        <Form layout={"inline"} form={underForm}>
-          <Form.Item>
-            <Space>
-              {/* <DataBindCheckbox></DataBindCheckbox> */}
-              <Checkbox onChange={(e)=>{setMyself(e.target.checked)}} checked={myself}>
-                查询操作人为您
-              </Checkbox>
-              <Radio.Group onChange={handleRadio} value={groupVal}>
-                <Radio value={1} >
-                  查询今天创建更新
-                </Radio>
-                <Radio value={2} >
-                  查询本周创建更新
-                </Radio>
-              </Radio.Group>
-              <Button icon={<SyncOutlined />}>
-                高级查询
+      <Form layout={"inline"} form={underForm}>
+        <Form.Item>
+          <Space>
+            {/* <DataBindCheckbox></DataBindCheckbox> */}
+            <Checkbox
+              onChange={(e) => {
+                setMyself(e.target.checked);
+                // console.log('1:',myself,e.target.checked)
+                if (e.target.checked) {
+                  setMyselfID(localStorage.getItem("_id"));
+                } else {
+                  // console.log('2',myself,e.target.checked)
+                  setMyselfID("");
+                }
+                // console.log('3')
+                // console.log('myself:',myself,' myselfID:',myselfID)
+              }
+            }
+            checked={myself}
+            >
+              查询操作人为您
+            </Checkbox>
+            <Radio.Group onChange={handleRadio} value={groupVal}>
+              <Radio value={1}>查询今天创建更新</Radio>
+              <Radio value={2}>查询本周创建更新</Radio>
+            </Radio.Group>
+            {/* <Button icon={<SyncOutlined />}>高级查询</Button> */}
+            <Form.Item>
+              <Button
+                htmlType="button"
+                onClick={() => {
+                  // topForm.resetFields();
+                  setMyself(false);
+                  setToday(false);
+                  setThisWeek(false);
+                  setMyselfID('');
+                  // console.log(myself, today, thisWeek);
+                  setGroupVal(0);
+                }}
+              >
+                重置
               </Button>
-              <Form.Item>
-                <Button htmlType="button" onClick={()=>{topForm.resetFields();setMyself(false);setToday(false);setThisWeek(false);console.log(myself,today,thisWeek);setGroupVal(0);Search()}}>重置</Button>
-              </Form.Item>
-              <Button type="primary" onClick={Search}>
-                查询
-              </Button>
-            </Space>
-          </Form.Item>
-        </Form>
-      </>
+            </Form.Item>
+            <Button type="primary" onClick={Search}>
+              查询
+            </Button>
+          </Space>
+        </Form.Item>
+      </Form>
+    </>
   );
 };
 const tableColumns = [
