86faa405a594bf95091051d074d083f215f11fac
BK201-Drama <1243532812@qq.com>
2022-05-05 11:40:38 +0800

    bugfix: 登录过期未自动登出

--- src/views/PageContainer/UserManage/UserManageAccount/components/BatchImportUserButton/BatchImportUserButton.js
+++ src/views/PageContainer/UserManage/UserManageAccount/components/BatchImportUserButton/BatchImportUserButton.js
@@ -13,29 +13,29 @@ const BatchImportUserButton = () => {
 
   const getXlsxData = (e) => {
     // 读取excel文件并写入到数组中
-    const files = e.target.files
-    if(files.length <= 0){
-      return false;
-    } else if (!/\.(xls|xlsx)$/.test(files[0].name.toLowerCase())){
-      alert('文件传格式不正确')
-      return false
-    }
-    const fileReader = new FileReader();
-    fileReader.readAsBinaryString(files[0]);
-    // 监听
-    fileReader.onload = (ev) => {
-      try {
-        const data = ev.target.result;
-        const workbook = xlsx.read(data, {
-          type: 'binary'
-        })
-        const wsname = workbook.SheetNames[0]//取第一张表
-        setXlsxData(xlsx.utils.sheet_to_json(workbook.Sheets[wsname]))//生成json表格内容
-        console.log('Json转化结果：', xlsxData)
-      } catch (e) {
-        return false
-      }
-    }
+    // const files = e.target.files
+    // if(files.length <= 0){
+    //   return false;
+    // } else if (!/\.(xls|xlsx)$/.test(files[0].name.toLowerCase())){
+    //   alert('文件传格式不正确')
+    //   return false
+    // }
+    // const fileReader = new FileReader();
+    // fileReader.readAsBinaryString(files[0]);
+    // // 监听
+    // fileReader.onload = (ev) => {
+    //   try {
+    //     const data = ev.target.result;
+    //     const workbook = xlsx.read(data, {
+    //       type: 'binary'
+    //     })
+    //     const wsname = workbook.SheetNames[0]//取第一张表
+    //     setXlsxData(xlsx.utils.sheet_to_json(workbook.Sheets[wsname]))//生成json表格内容
+    //     console.log('Json转化结果：', xlsxData)
+    //   } catch (e) {
+    //     return false
+    //   }
+    // }
   }
 
   return (
