9dcbe4e8cb7e9ff55744faae660f81b9c366bc2d
张奕凯 <1781315832@qq.com>
2022-05-25 16:46:05 +0800

    重新提交

--- src/api/systemBackup.js
+++ src/api/systemBackup.js
@@ -16,6 +16,20 @@ const api ={
             data
         })
     },
+    HandleBackup(data){
+        return service.request({
+            method:"get",
+            url:"v1/handle-backup",
+            data
+        })
+    },
+    GetBackup(data){
+        return service.request({
+            method:"get",
+            url:"v1/mongo-backup",
+            data
+        })
+    }
 }
 
 export default api
