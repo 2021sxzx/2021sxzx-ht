6feb9dedea497a8e953eb249345043d71ef8c61d
UmyNya <1079102275@qq.com>
2022-06-12 13:09:39 +0800

    feat(login): 增加了输入安全检查，增加了提示性文字并使用 useForm 优化表单逻辑，移除了不需要的第三方登录图标,注释掉了了记住密码部分无用的代码

--- src/views/login/components/PasswordLoginForm.js
+++ src/views/login/components/PasswordLoginForm.js
@@ -1,7 +1,7 @@
 import {Button,  Col, Form, Row, Input, message} from "antd";
 import {LockOutlined, UserOutlined} from "@ant-design/icons";
-import React, {useState} from "react";
-import QuickLogin from "./QuickLogin";
+import React from "react";
+// import QuickLogin from "./QuickLogin";
 import api from "../../../api/login";
 import UrlJump from "../../../utils/UrlJump";
 
@@ -13,69 +13,50 @@ import UrlJump from "../../../utils/UrlJump";
 export default function PasswordLoginForm() {
     // const rememberPassword = !!localStorage.getItem('rememberPassword') // 是否选择记住密码（Boolean）
     const historyAccount = localStorage.getItem('account')
-    const historyPassword = localStorage.getItem('password')
+    // const historyPassword = localStorage.getItem('password')
 
-    const [account, setAccount] = useState(historyAccount)
-    const [password, setPassword] = useState(historyPassword)
+    // const [account, setAccount] = useState(historyAccount)
+    // const [password, setPassword] = useState(historyPassword)
 
-    const handleInputChangeAccount = (e) => {
-        setAccount(e.target.value)
-    }
+    // const handleInputChangeAccount = (e) => {
+    //     setAccount(e.target.value)
+    // }
 
-    const handleInputChangePassword = (e) => {
-        setPassword(e.target.value)
-    }
+    // const handleInputChangePassword = (e) => {
+    //     setPassword(e.target.value)
+    // }
 
-    const login = () => {
+    const onFinish = (values) => {
         api.Login({
-            account: account,
-            password: password
+            account: values.account,
+            password: values.password
         }).then(async response => {
             // 保存用户信息：账号密码用户id
-            await saveUserInfo(response)
+            await saveUserInfo(response,values)
 
             // 展现 0.1s 的登录成功操作提示并自动跳转到首页
             message.success('登录成功', 0.1, () => {
                 UrlJump.goto('#/home')
             })
-        }).catch(() => {
-            // TODO（钟卓江）：细化报错信息
-            message.error('请尝试重新登录，或者检查账号密码或者网络状态')
+        }).catch((error) => {
+            message.error(error.response.data)
         })
     }
 
-    /**
-     * HACK：出于安全考虑，不再需要保存密码
-     * 是否记住账号密码。
-     * 信息保存到 localStorage：true 存非空字符串，false 存空字符串 ''
-     * @param event
-     */
-    // const handleCheckboxChange = (event) => {
-    //     // 将是否记住账号密码保存到 localStorage：true 存非空字符串，false 存空字符串 ''
-    //     localStorage.setItem('rememberPassword',event.target.checked?'rememberPassword':'')
-    // }
+    const onFinishFailed = () => {
+        message.warn('请正确填写账号密码')
+    }
 
     /**
      * 在 localstorage
      * @param response 响应报文
+     * @param values 表单中的数据
      */
-    const saveUserInfo = (response) => {
+    const saveUserInfo = (response,values) => {
         // 保存账号
-        localStorage.setItem('account', account)
+        localStorage.setItem('account', values.account)
         localStorage.setItem('_id', response.data.data._id);
-        // localStorage.setItem('loginToken', response.data.data.jwt.token)
-        // localStorage.setItem('role_name', response.data.data.role_name)
-        // localStorage.setItem('roleName', response.data.data.role_name)
         localStorage.setItem('roleID', response.data.data.role_id)
-
-        // HACK：出于安全考虑，不再需要保存密码
-        // 从 localStorage 中获取是否记住密码（boolean）
-        // let rememberPassword = !!localStorage.getItem('rememberPassword')
-        // if(rememberPassword){
-        //     localStorage.setItem('password',password)
-        // }else{
-        //     localStorage.removeItem('password')
-        // }
     }
 
     return (
@@ -92,6 +73,8 @@ export default function PasswordLoginForm() {
                 remember: true,
             }}
             autoComplete="off"
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
         >
             {/*账号/手机号码*/}
             <Form.Item
@@ -102,15 +85,20 @@ export default function PasswordLoginForm() {
                         required: true,
                         message: '请输入手机号码!',
                     },
+                    {
+                        // TODO：暂时保留了开发人员账号
+                        pattern:/^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\d{8}|account$/,
+                        message:'请输入正确的手机号码或者开发人员账号'
+                    },
                 ]}
             >
                 <Input placeholder={'手机号码'}
                        defaultValue={historyAccount}
-                       // type={"number"}
-                       maxLength={15}
+                       maxLength={32}
                        prefix={<UserOutlined className="site-form-item-icon"/>}
                        allowClear={true}
-                       onChange={handleInputChangeAccount}/>
+                       // onChange={handleInputChangeAccount}
+                />
             </Form.Item>
             {/*密码*/}
             <Form.Item
@@ -121,15 +109,29 @@ export default function PasswordLoginForm() {
                         required: true,
                         message: '请输入密码!',
                     },
+                    {
+                        min:8,
+                        message:'密码长度要求不小于 8 位'
+                    },
+                    {
+                        max:32,
+                        message:'密码长度要求不大于 32 位'
+                    },
+                    {
+                        // TODO：暂时保留开发人员密码
+                        pattern:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%&*_\-+=])[\w\d@#$%&*_\-+=]*|password$/,
+                        message:'要求同时使用大小写字母，数字和部分特殊字符(@#$%&*_+-=)，不支持空格'
+                    }
                 ]}
             >
                 <Input.Password placeholder={"密码"}
-                                defaultValue={historyPassword}
+                                // defaultValue={historyPassword}
                                 type={"password"}
                                 maxLength={32}
                                 prefix={<LockOutlined className="site-form-item-icon"/>}
                                 allowClear={true}
-                                onChange={handleInputChangePassword}/>
+                                // onChange={handleInputChangePassword}
+                />
             </Form.Item>
             {/*是否记住登录状态 & 忘记密码 */}
             <Form.Item
@@ -150,24 +152,35 @@ export default function PasswordLoginForm() {
                 {/*        </a>*/}
                 {/*    </Col>*/}
                 {/*</Row>*/}
-                <a className="login-form-forgot" href="">
-                    忘记密码
+
+                {/*TODO：忘记密码待完成*/}
+                <a
+                    className="login-form-forgot"
+                    href=""
+                    style={{float:"right"}}
+                >
+                    忘记密码（开发中）
                 </a>
             </Form.Item>
             {/*登录按钮*/}
             <Form.Item>
                 <Row type="flex" justify="center" align="middle">
                     <Col span={24}>
-                        <Button type="primary" htmlType="submit" block={true} onClick={login}>
+                        <Button
+                            type="primary"
+                            htmlType="submit"
+                            block={true}
+                            // onClick={login}
+                        >
                             登录
                         </Button>
                     </Col>
                 </Row>
             </Form.Item>
-            <Form.Item>
-                {/*第三方快捷登录*/}
-                <QuickLogin/>
-            </Form.Item>
+            {/*<Form.Item>*/}
+            {/*    /!*第三方快捷登录*!/*/}
+            {/*    <QuickLogin/>*/}
+            {/*</Form.Item>*/}
         </Form>
     )
 }
