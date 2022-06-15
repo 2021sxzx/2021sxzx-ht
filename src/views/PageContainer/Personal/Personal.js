aa434310ad84ec6db1dc2b40c4ca5cd15cb2824f
UmyNya <1079102275@qq.com>
2022-06-15 19:47:15 +0800

    feat(personal): 实现数据的对接

--- src/views/PageContainer/Personal/Personal.js
+++ src/views/PageContainer/Personal/Personal.js
@@ -1,60 +1,49 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import style from './Personal.module.scss'
 import {Avatar, Card, Image, message} from "antd";
 import {EditOutlined,} from "@ant-design/icons";
 import Meta from "antd/es/card/Meta";
 import PersonalDescription from "./components/PersonalDescription";
 import UserModal from "../UserManage/UserManageAccount/components/UserModal";
+import apiPersonal from "../../../api/personal";
 
 
 function Personal() {
+    // 用户信息
+    const [userInfo, setUserInfo] = useState({});
+    useEffect(() => {
+        apiPersonal.getTopHeaderData()
+            .then(value => {
+                console.log(value.data)
+                setUserInfo(value.data.data)
+            })
+            .catch(err => {
+                console.log(err)
+            });
+    }, []);
 
     return (
         <div className={style.cardContainer}>
             <Card
                 className={style.cardStyle}
-                // cover={
-                //     <div>
-                //         {/*<img*/}
-                //         {/*    alt="example"*/}
-                //         {/*    src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"*/}
-                //         {/*/>*/}
-                //         <Avatar
-                //             size={{
-                //                 xs: 48,
-                //                 sm: 64,
-                //                 md: 80,
-                //                 lg: 128,
-                //                 xl: 160,
-                //                 xxl: 200,
-                //             }}
-                //             src="https://joeschmoe.io/api/v1/random"
-                //
-                //         />
-                //     </div>
-                // }
                 extra={(
-                    // <Button icon={<EditOutlined key="edit"/>} type={'text'} size={'large'}/>
                     <UserModal
-                        // buttonText={'修改用户信息'}
                         buttonProps={{
-                            // disabled: false,
                             type: 'text',
                             shape: "circle", // 按钮形状
                             icon: <EditOutlined/>, // 按钮图标
                             size: 'large',
                         }}
                         tooltipSuccessTitle={'编辑个人信息'}
-                        // tooltipErrorTitle={'不能修改同级别角色的用户信息'}
                         title={'编辑个人信息'}
                         detailData={{
-                            user_name: '小王',
-                            account: '18128706873',
-                            password: '11aaAA@@',
-                            role_name: 'role_name',
-                            role_id: 123,
-                            unit_id: 123,
-                            unit_name: 'unit_name',
+                            user_name: userInfo.user_name,
+                            account: userInfo.account,
+                            password: userInfo.password,
+                            role_name: userInfo.role_name,
+                            role_id: userInfo.role_id,
+                            unit_id: userInfo.unit_id,
+                            unit_name: userInfo.unit_name,
                         }}
                         saveInfoFunction={() => {
                             message.success('修改个人信息成功（test）')
@@ -82,60 +71,19 @@ function Personal() {
                             display: "inline",
                             fontSize: 20,
                             margin: '15px',
-                        }}>小王
+                        }}>
+                            {userInfo.user_name}
                         </div>
                     </div>
                 )}
-
-                // actions={[
-                //     // <SettingOutlined key="setting" />,
-                //     <EditOutlined key="edit" />,
-                //     // <EllipsisOutlined key="ellipsis" />,
-                // ]}
             >
                 <Meta
-                    // avatar={<Avatar src="https://joeschmoe.io/api/v1/random"/>} // 头像
-                    // title={ (
-                    //         <div>
-                    //             个人资料
-                    //             <EditOutlined key="edit"/>
-                    //         </div>
-                    //     )
-                    // } // 标题
                     description={(
-                        <PersonalDescription/>
+                        <PersonalDescription data={userInfo}/>
                     )}
                     className={style.cardMeta}
                 />
             </Card>
-            {/*<div style={{*/}
-            {/*    float:"right",*/}
-            {/*    display:"flex",*/}
-            {/*    alignItems:"center",*/}
-            {/*}}>*/}
-            {/*    <div style={{*/}
-            {/*        display: 'inline-block',*/}
-            {/*        margin:'25px',*/}
-            {/*    }}>*/}
-            {/*        <Avatar*/}
-            {/*            size={{*/}
-            {/*                xs: 48,*/}
-            {/*                sm: 64,*/}
-            {/*                md: 80,*/}
-            {/*                lg: 128,*/}
-            {/*                xl: 160,*/}
-            {/*                xxl: 200,*/}
-            {/*            }}*/}
-            {/*            src="https://joeschmoe.io/api/v1/random"*/}
-
-            {/*        />*/}
-            {/*    </div>*/}
-            {/*    <div style={{*/}
-            {/*        display: 'inline-block'*/}
-            {/*    }}>*/}
-            {/*        <PersonalDescription/>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
         </div>
     )
 }
