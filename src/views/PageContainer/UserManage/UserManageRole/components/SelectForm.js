48bff057fda38be611c3f9128ede352c172e2caf
UmyNya <1079102275@qq.com>
2022-06-27 16:35:04 +0800

    fix(roleManagement): 修复了搜索内容不符合要求缺仍然能进行搜索的 bug

--- src/views/PageContainer/UserManage/UserManageRole/components/SelectForm.js
+++ src/views/PageContainer/UserManage/UserManageRole/components/SelectForm.js
@@ -1,5 +1,5 @@
 import {Button, Form, Input, message} from "antd";
-import React, {useState} from "react";
+import React, {useCallback} from "react";
 import api from "../../../../../api/role";
 import RoleModal from "./RoleModal";
 
@@ -15,63 +15,72 @@ import RoleModal from "./RoleModal";
 function SelectForm(props) {
     // 使用并设置表单组件
     const [form] = Form.useForm();
-    const formLayout = 'inline';
-
-    // roleName 为状态值，setRoleName 为更新 roleName 的方法
-    const [searchValue, setSearchValue] = useState('');
-
-    // 保存输入框的角色名称
-    const handleInputChange = (e) => {
-        setSearchValue(e.target.value)
-    }
 
     // 搜索对应的角色
     const Search = () => {
-        const data = {
-            searchValue: searchValue
-        }
-
-        props.getSearch(data)
+        form.validateFields().then(() => {
+            props.getSearch({
+                searchValue: form.getFieldValue('searchValue')
+            })
+        }).catch(() => {
+        })
     }
 
-    const AddRoleAndRefresh = (data) => {
+    const AddRoleAndRefresh = useCallback((data) => {
         // 新增角色
         api.AddRole({
             role_name: data.role_name,
             role_describe: data.role_describe,
             permission_identifier_array: data.permission_identifier_array,
         }).then(() => {
-            message.success('添加角色成功')
+            message.success('创建角色成功')
         }).catch(() => {
-            message.error('添加角色出现错误')
+            message.error('创建角色出现错误')
         }).finally(() => {
             props.refreshTableData()
         })
-    }
+    }, [props.refreshTableData])
 
     return (
         <>
             <Form
-                layout={formLayout}
+                layout={'inline'}
                 form={form}
                 initialValues={{
-                    layout: formLayout,
+                    searchValue: '',
                 }}
+                onFinish={Search}
             >
                 <Form.Item>
-                    <RoleModal buttonText={'新增角色'} buttonType={'primary'} title={'新增角色'}
-                               detailData={{
-                                   role_name: '',
-                                   role_describe: '',
-                                   permission: [],
-                                   permission_identifier_array: [],
-                               }} callback={AddRoleAndRefresh}/>
+                    <RoleModal
+                        buttonText={'创建角色'}
+                        buttonType={'primary'}
+                        title={'新增角色'}
+                        detailData={{
+                            role_name: '',
+                            role_describe: '',
+                            permission: [],
+                            permission_identifier_array: [],
+                        }}
+                        callback={AddRoleAndRefresh}/>
+                </Form.Item>
+                <Form.Item
+                    name={'searchValue'}
+                    rules={[
+                        {
+                            max: 64,
+                            message: '搜索内容请小于64个字',
+                        },
+                    ]}
+                >
+                    <Input
+                        placeholder="请输入角色名称或描述"
+                        size="middle"
+                        allowClear
+                    />
                 </Form.Item>
                 <Form.Item>
-                    <Input placeholder="请输入角色名称或描述" size="middle" onChange={handleInputChange}/>
-                </Form.Item>
-                <Form.Item>
-                    <Button type="primary" onClick={Search}>搜索</Button>
+                    <Button type="primary" htmlType={'submit'}>搜索</Button>
                 </Form.Item>
             </Form>
         </>
