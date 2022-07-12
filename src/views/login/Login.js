d9545064aed2c3c15acbdfea1a53fb842186b22d
UmyNya <1079102275@qq.com>
2022-07-12 17:07:36 +0800

    refactor(login): 调整登录页面的样式

--- src/views/login/Login.js
+++ src/views/login/Login.js
@@ -4,25 +4,26 @@ import {Layout} from 'antd';
 
 import LoginForm from "./components/LoginForm";
 import PageFooter from "./components/PageFooter";
+import style from './login.module.scss'
 
 const {Header, Footer, Content} = Layout;
 
 export default function Login() {
     return (
         <div>
-            <Layout>
+            <Layout className={style.loginLayout}>
                 {/*页头*/}
                 <Header>
                     <font color={"white"}>广州人社局智能咨询平台</font>
                 </Header>
 
                 {/*登录页主体内容*/}
-                <Content>
+                <Content className={style.container}>
                     <LoginForm/>
                 </Content>
 
                 {/*页尾*/}
-                <Footer style={{background: '#FFFF'}}>
+                <Footer className={style.footer}>
                     <PageFooter/>
                 </Footer>
             </Layout>
