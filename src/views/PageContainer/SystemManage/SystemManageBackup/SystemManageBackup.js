ed9c2c10b9df9b8d05df968ab1136da0d667ba40
lhy <944371430@qq.com>
2022-07-18 18:02:46 +0800

    fix: 完善数据备份
    修复手动、自动备份功能
    修复每页展示数无法更改的缺陷
    修正自动备份的单位为天
    显示每个备份文件的大小。当服务器返回错误信息时，显示在备份文件大小列中

--- src/views/PageContainer/SystemManage/SystemManageBackup/SystemManageBackup.js
+++ src/views/PageContainer/SystemManage/SystemManageBackup/SystemManageBackup.js
@@ -1,361 +1,154 @@
-import React, { useEffect, useState } from "react";
-//
+import React, {useEffect, useState} from "react";
 import {
-  Space,
-  Form,
-  Input,
-  PageHeader, Button, Descriptions,Table,Pagination,
-  Checkbox,
-  Radio,
-  Divider,
-  Modal,
-  Tabs,
-  DatePicker,
-  Alert,
-  message,
+  Space, Form, Button, Table, Tabs, message, InputNumber,
 } from "antd";
-import { getYMD, getTimeStamp } from "../../../../utils/TimeStamp";
-import { SyncOutlined } from "@ant-design/icons";
 import api from "../../../../api/systemBackup.js";
-// import './SystemManageBackup.css'
-const { TabPane } = Tabs;
-const { RangePicker } = DatePicker;
 
-const SelectForm = (props) => {
+const {TabPane} = Tabs;
+
+// 变量
+// 用于记录被选中行
+let selectedRows = null;
+
+// 方法
+// 删除操作
+const deleteBackup = async (aimedRowData) => {
+  api.DeleteSystemBackup(aimedRowData).then();
+};
+
+// 组件声明
+// SelectForm 备份周期表单
+const SelectForm = () => {
+  // 变量
+  // 表单数据
   const [form] = Form.useForm();
-/*  const [startTime, setStartTime] = useState("");
-  const [endTime, setEndTime] = useState("");
-  const [myself, setMyself] = useState(false);
-  const [today, setToday] = useState(false);
-  const [thisWeek, setThisWeek] = useState(false);
 
-  const handleToday = (e) => {
-    setToday(true);
-    setThisWeek(false);
-    console.log("today:", today, ".week:", thisWeek);
-  };
-  const handleWeek = (e) => {
-    setToday(false);
-    setThisWeek(true);
-    console.log("week:", thisWeek, ".today:", today);
-  };
-  const handleRadio = (e) => {
-    console.log("radio checked", e.target.value);
-    if (e.target.value === 1) {
-      handleToday();
-    } else if (e.target.value === 2) {
-      handleWeek();
-    }
-  };
-  const Search = () => {
-    const data = {
-      myself,
-      today,
-      thisWeek,
-    };
-    props.getSearch(data);
-  };
-  const handleDateChange = (value, dateString) => {
-    if (value) {
-      setStartTime(getTimeStamp(dateString[0]));
-      setEndTime(getTimeStamp(dateString[1]));
-    } else {
-      setEndTime("");
-      setStartTime("");
-    }
-  };*/
-  const onFinish=(values)=>{
-    // console.log('Success:',values)
-    api.ChangeBackupCycle(values).then((res)=>{
-      if(res.data.data==='success'){message.success(res.data.msg)}
-      else{message.error(res.data.msg)}
-    })
-    // console.log(form.getFieldValue())
-  }
-/*  const onFinishFailed = (errorInfo) => {
-    console.log('Failed:', errorInfo);
-  }*/
-  const backupNow=()=>{
-    api.HandleBackup().then(res=>{
-      message.success('备份成功')
+  // 方法
+  // 提交表单，修改备份周期
+  const onFinish = (values) => {
+    api.ChangeBackupCycle(values).then((res) => {
+      /** @property res.data.msg */
+      if (res.data.data === 'success') {
+        message.success(res.data.msg).then();
+      } else {
+        message.error(res.data.msg).then();
+      }
     })
   }
+  // 手动备份按钮的动作，立即向服务器发送手动备份请求
+  const backupNow = () => {
+    api.HandleBackup().then(() => {
+      message.success('备份成功，因备份时间较长，请稍后刷新页面查看备份').then();
+    })
+  }
+
+  // 组件初始化
+  // 获取备份周期
   useEffect(() => {
-    api.GetBackupCycle().then((res)=>{
-      // console.log('-----------------------')
-      // console.log(res)
+    api.GetBackupCycle().then((res) => {
       form.setFieldsValue({
-        time:res.data.data
+        time: res.data.data
       })
     })
   })
-  return (
-    <>
-      <Form name="setTime" form={form} layout={"inline"} onFinish={onFinish}>
-        <Form.Item>
-              <Button type="primary" onClick={backupNow}>
-                手动备份
-              </Button>
-        </Form.Item>
-        <Form.Item>
-          每
-        </Form.Item>
-        <Form.Item name="time">
-          <Input //addonBefore="每" addonAfter="小时系统自动备份一次"
-            style={{ width: "40px" }}
-          ></Input>
-        </Form.Item>
-        <Form.Item>
-          小时系统自动备份一次
-        </Form.Item>
-        <Form.Item>
-              <Button type="primary" htmlType="submit">
-                确认更改
-              </Button>
-        </Form.Item>
-      </Form>
-    </>
-  );
-};
 
-const tableData1 = [
-  {
-    key: 1,
-    failure_name: "闪退",
-    create_time: "2017-01-01",
-    content: "闪选",
-    user_name: "zyk",
-  },
-  {
-    key: 2,
-    failure_name: "重启",
-    create_time: "2017-08-01",
-    content: "dddd",
-    user_name: "wlz",
-  },
-  {
-    key: 3,
-    failure_name: "崩溃",
-    create_time: "2017-10-01",
-    content: "噢哟",
-    user_name: "lyh",
-  },
-];
-const pagination1=()=>{
-  return (
-    <Pagination size="small" total={50} showSizeChanger showQuickJumper />
-  )
-}
-//弹窗Modal
-const HandleModal = (props) => {
-  const [isModalVisible, setIsModalVisible] = useState(false);
-
-  const showModal = () => {
-    setIsModalVisible(true);
-  };
-
-  const handleOk = () => {
-    setIsModalVisible(false);
-  };
-
-  const handleCancel = () => {
-    setIsModalVisible(false);
-  };
-
-  return (
-    <>
-      <Button style={{ border: "1px solid blue" }} onClick={showModal}>
-        处理
-      </Button>
-      <Modal
-        title="Basic Modal"
-        visible={isModalVisible}
-        onOk={handleOk}
-        onCancel={handleCancel}
-        okText="确定"
-        cancelText="取消"
-      >
-        <p>{props.record.failure_name}</p>
-        <p>{props.record.user_name}</p>
-        <p>{props.record.create_time}</p>
-      </Modal>
-    </>
-  );
-};
-
-//删除操作
-const deleteFuncElem = async (aimedRowData) => {
-  // console.log(aimedRowData);
-  api.DeleteSystemBackup(aimedRowData).then(message.success('删除故障成功.'));
-/*   const totalFuncDataList = tableData1;
-  let i;
-  let indexOfFuncList;
-  for (i = 0; i < totalFuncDataList.length; i++) {
-    if (aimedRowData.index === totalFuncDataList[i].index) {
-      break;
-    }
-  }
-  totalFuncDataList.splice(i + 1, 1);
-  console.log(totalFuncDataList); */
-  // message.success(aimedRowData)
-  // this.setState({
-  //   data:totalFuncDataList
-  // });
-  // this.showTable(this.state.data);
-};
-
-const tableColumns = [
-  {
-    title: "备份名称",
-    dataIndex: "backup_name",
-    key: "backup_name",
-  },
-  {
-    title: "备份人",
-    key: "user_name",
-    dataIndex: "user_name",
-  },
-  {
-    key: "handle",
-    dataIndex: "handle",
-    render: (
-      _,
-      record,
-      index //参数分别为当前行的值，当前行数据，行索引
-    ) => (
-      <>
-        {/* <Space size="middle">
-        <button onClick={()=>{console.log(index)}}>处理</button>
-      </Space> */}
-        {/* <HandleModal record={record}></HandleModal> */}
-        <Space size="middle">
-        <Button
-          style={{ border: "1px solid blue" }}
-          onClick={() => {
-            deleteFuncElem(record);
-          }}
-        >
-          下载
+  return (<>
+    <Form name="setTime" form={form} layout={"inline"} onFinish={onFinish}>
+      <Form.Item>
+        <Button type="primary" onClick={backupNow}>
+          手动备份
         </Button>
-      </Space>
-      </>
-
-    ),
-  },
-  {
-    key: "delete",
-    dataIndex: "delete",
-    render: (_, record) => (
-      <Space size="middle">
-        <Button
-          style={{ border: "1px solid blue" }}
-          onClick={() => {
-            deleteFuncElem(record);
-            message.success('删除成功')
-          }}
-        >
-          删除
+      </Form.Item>
+      <Form.Item>
+        每
+      </Form.Item>
+      <Form.Item name="time">
+        <InputNumber min={1}/>
+      </Form.Item>
+      <Form.Item>
+        天系统自动备份一次
+      </Form.Item>
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
+          确认更改
         </Button>
-      </Space>
-    ),
-  },
-];
-
-const rowSelection = {
-  onChange: (selectedRowKeys, selectedRows) => {
-    // console.log(
-    //   `selectedRowKeys: ${selectedRowKeys}`,
-    //   "selectedRows: ",
-    //   selectedRows
-    // );
-  },
-  getCheckboxProps: (record) => ({
-    disabled: record.name === "Disabled User",
-    // Column configuration not to be checked
-    name: record.name,
-  }),
+      </Form.Item>
+    </Form>
+  </>);
 };
 
-const Demo = () => {
-  const [visible, setVisible] = useState(false);
-  const [selectionType, setSelectionType] = useState('checkbox');
-  const [tableData, setTableData] = useState('');
-  const getBackup = (data) => {
-    api
-      .GetBackup(data)
-      .then((response) => {
-        setTableData(response.data.data);
-        // console.log("response.data.data=", response.data.data);
-      })
-      .catch((error) => {});
+// 导出默认组件
+export default function SystemManageBackup() {
+  // 变量
+  // 用于展示备份数据的表格
+  const [tableData, setTableData] = useState();
+
+  // 方法
+  // 获取备份信息
+  const getBackup = () => {
+    api.GetBackup().then(response => {
+      setTableData(response.data.data);
+    });
   };
-  useEffect(() => {
-    getBackup()
-  },[]);
-  return (
-    <>
-      {visible ? (
-        <Alert message="Warning" type="warning" showIcon closable />
-      ) : null}
+
+  // 组件初始化
+  // 获取备份信息
+  useEffect(getBackup, []);
+
+  return <>
+    <div id="backup">
       <div className="card-container">
         <Tabs
           type="card"
-          tabBarExtraContent={<SelectForm setVisible={setVisible}></SelectForm>}
+          tabBarExtraContent={<Space>
+            <Button onClick={() => {
+              if (selectedRows) Promise.all(selectedRows.map(row => deleteBackup(row))).then(() => {
+                getBackup();
+                message.success("批量删除备份成功").then();
+              });
+              else message.info("请选择要删除的备份").then();
+            }}>批量删除</Button>
+            <SelectForm/>
+          </Space>}
         >
           <TabPane tab="数据库备份" key="1">
             <Table
-              rowKey="log_id"
-              // rowSelection={{
-              //   type: selectionType,
-              //   ...rowSelection,
-              // }}
-              columns={tableColumns}
+              rowKey="_id" // by lhy 来自后人的一个 :) 明明数据返回的是 _id 硬生生写 log_id 成功让我在 rowSelection 卡了半天
+              columns={[{title: "备份名称", dataIndex: "backup_name"}, {title: "备份人", dataIndex: "user_name"}, {
+                title: "备份文件大小", dataIndex: "file_size", render: (_, record) => {
+                  // 服务器返回一个数字，表示备份文件的字节数，展示时使用合适单位
+                  if (typeof record['file_size'] === 'number') {
+                    const power = Math.min(4, Math.floor(Math.log2(record['file_size']) / 10));
+                    return `${(record['file_size'] / Math.pow(1024, power)).toFixed(2)} ${['B', 'KB', 'MB', 'GB'][power]}`
+                  }
+                  return record['file_size'];
+                }
+              }, {
+                key: "delete", dataIndex: "delete", render: (_, record) => <Button
+                  style={{border: "1px solid blue"}}
+                  onClick={() => {
+                    deleteBackup(record).then(message.success('删除故障成功.'));
+                    getBackup();
+                  }}
+                >删除</Button>
+              }]}
               dataSource={tableData}
-              pagination={{ pageSize: 10,showQuickJumper:true,pageSizeOptions:[10,20,50],showSizeChanger:true }}
+              pagination={{
+                // pageSize: 10, // by lhy 写了 pageSize 就要写 onShowSizeChange 回调啊
+                showQuickJumper: true,
+                pageSizeOptions: [10, 20, 50],
+                showSizeChanger: true
+              }}
+              rowSelection={{
+                type: "checkbox", onChange(_, selectedRows_) {
+                  selectedRows = selectedRows_;
+                  console.log(selectedRows);
+                }
+              }}
             />
           </TabPane>
-          {/* <TabPane tab="文件备份" key="2">
-            <p>Content of Tab Pane 2</p>
-            <p>Content of Tab Pane 2</p>
-            <p>Content of Tab Pane 2</p>
-          </TabPane>
-          <TabPane tab="日志备份" key="3">
-            <p>Content of Tab Pane 3</p>
-            <p>Content of Tab Pane 3</p>
-            <p>Content of Tab Pane 3</p>
-          </TabPane> */}
         </Tabs>
       </div>
-    </>
-  );
-};
-export default function SystemManageBackup() {
-  // const [tableData, setTableData] = useState([]);
-  // const getLog = (data) => {
-  //   api
-  //     .GetLog(data)
-  //     .then((response) => {
-  //       setTableData(response.data.data);
-  //       console.log("response.data.data=", response.data.data);
-  //     })
-  //     .catch((error) => {});
-  // };
-  // const getSearchLog = (data) => {
-  //   console.log(data);
-  //   api
-  //     .SearchLog(data)
-  //     .then((response) => {
-  //       console.log("searchData=", response.data.data);
-  //       setTableData(response.data.data);
-  //     })
-  //     .catch((error) => {});
-  // };
-  // useEffect(() => {
-  //   getLog({});
-  // }, []);
-  return (
-    <div id="backup">
-      <Demo></Demo>
     </div>
-  );
+  </>;
 }
