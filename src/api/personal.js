ffcd41865becfb4f36a1454bc9b000c0534668d1
Gragonballer <1252034170@qq.com>
2022-07-18 16:40:19 +0800

    feat:添加梦网科技的接口

--- src/api/personal.js
+++ src/api/personal.js
@@ -6,6 +6,15 @@ const api = {
             method: 'get',
             url: '/v1/personal'
         });
+    },
+    //梦网科技的接口
+    modifyPassword(data){
+        console.log(data)
+        return axios.request({
+            method: 'post',
+            url: '/v2/modifyPwd',
+            data:data
+        });
     }
 }
 
