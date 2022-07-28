4215f1b1c75b34e47004eb4d9fa0bdd17403176e
lhy <944371430@qq.com>
2022-07-28 17:45:28 +0800

    feat: 热词管理功能

--- src/api/systemBasic.js
+++ src/api/systemBasic.js
@@ -3,5 +3,8 @@ import service from './http'
 const request = (method, url, data) => service.request({method, url, data})
 export default {
   getTel: () => request('get', '/v1/get-tel'),
-  setTel: data => request('post', '/v1/set-tel', data)
+  setTel: data => request('post', '/v1/set-tel', data),
+  getHotKeys: () => request('get', 'v1/get-hot-keys'),
+  deleteHotKey: data => request('post', 'v1/delete-hot-key', data),
+  addHotKey: data => request('post', 'v1/add-hot-key', data)
 }
