9e95adc749d81915102db15165b4b3cf4105c453
UmyNya <1079102275@qq.com>
2022-10-19 11:07:21 +0800

    feat: 更换了登录界面人社局图标

--- src/views/login/components/HeaderLoginForm.js
+++ src/views/login/components/HeaderLoginForm.js
@@ -12,17 +12,24 @@ export default function HeaderLoginForm() {
             <Row type="flex" justify="center" align="middle">
                 <Col>
                     <img
-                        width={200}
-                        src="https://gw.alipayobjects.com/zos/antfincdn/aPkFc8Sj7n/method-draw-image.svg"
+                        width={300}
+                        // src="https://gw.alipayobjects.com/zos/antfincdn/aPkFc8Sj7n/method-draw-image.svg"
+                        src={'http://8.134.73.52/public/imgs/ic_logo.png'}
                         alt=""
+                        style={{
+                            // 让 LOGO 视觉上居中
+                            marginRight:'50px',
+                            marginBottom:'30px',
+                            marginTop:'30px',
+                        }}
                     />
                 </Col>
             </Row>
-            <Row type="flex" justify="center" align="middle">
-                <Col>
-                    <div>广州人社局智能咨询平台</div>
-                </Col>
-            </Row>
+            {/*<Row type="flex" justify="center" align="middle">*/}
+            {/*    <Col>*/}
+            {/*        <div>广州人社局智能咨询平台</div>*/}
+            {/*    </Col>*/}
+            {/*</Row>*/}
         </>
     )
 }
