1137be4c1fa5cf614a7e699d9aa523bca19b60d6
iSonce <705213278@qq.com>
2023-01-13 07:14:39 +0800

    fix(Login): 使用LocalStorage存储下一次可发送验证码时间, 界面刷新后仍然计时

--- src/views/login/components/CountDownButton.js
+++ src/views/login/components/CountDownButton.js
@@ -1,7 +1,6 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useState, useMemo} from "react";
 import {Button, message} from "antd";
 import api from "../../../api/login";
-import { duration } from "moment";
 /**
  * 用于发送手机验证码的倒计时按钮
  *
@@ -9,60 +8,62 @@ import { duration } from "moment";
  * @constructor
  */
 export default function CountDownButton(props) {
-    // 初始化倒计时的常数属性和设置倒计时状态
-    const maxCount = 60
-    const toSendText = '获取验证码'
+    const maxLockSecond = 60
+    const [lockSecond,setLockSecond] = useState(0)
 
-    // console.log(props)
-    const [countDownStatus, setCountDownStatus] = useState({
-        count: maxCount,
-        text: toSendText,
-        isGetting: false,
-    })
+    // 按钮文本
+    const buttonText = useMemo(
+        () => (lockSecond === 0 ? `获取验证码` : `${lockSecond}s后重发`),
+        [lockSecond]
+    )
+    // 是否锁定按钮
+    // TODO: 需要父组件提供一个布尔值用于判断手机号码是否正确，手机号码错误时也需要锁定
+    const buttonLock = useMemo(() => lockSecond > 0, [lockSecond]);
 
-    // 重置倒计时状态
-    const resetCountDownStatus = () => {
-        setCountDownStatus({
-            count: maxCount,
-            text: toSendText,
-            isGetting: false,
-        })
-    }
+    // 初次启动时查询localStorage判断是否继续计时
+    useEffect(()=>{
+        let lockEnd = localStorage.getItem('sxzx-ht-sendVC-lockEnd')
+        if(lockEnd === null || lockEnd === undefined){
+            // do nothing
+        }else{
+            let LeftoverLockSecond = parseInt((lockEnd - new Date().getTime())/1000)
+            if(LeftoverLockSecond > 0){
+                // 还未到解锁时间，开始倒计时
+                setLockTimer(LeftoverLockSecond)
+            }else{
+                // 已到达解锁时间可将localStorage中的字段删除
+                localStorage.removeItem('sxzx-ht-sendVC-lockEnd')
+            }
+        }
+    },[])
 
-    // 倒计时中更新计时状态
-    const countDowning = () => {
-        setCountDownStatus({
-            count: countDownStatus.count--,
-            text: countDownStatus.count + 's 后重发',
-            isGetting: true,
-        })
-    }
-
-    // 设置倒计时
-    const countDown = () => {
-        countDowning()
-
-        const intervalID = setInterval(() => {
-            if (countDownStatus.count < 1) {
-                resetCountDownStatus()
+    // 设置计时器
+    const setLockTimer = (second) => {
+        setLockSecond(second)
+        const timer = setInterval(() => {
+            second--
+            setLockSecond(second)
+            if (second < 1) {
+                // 已到达解锁时间可将localStorage中的字段删除
+                localStorage.removeItem('sxzx-ht-sendVC-lockEnd')
                 // 清除定时器
-                clearInterval(intervalID)
-                console.log('countdown', countDownStatus.count)
-            } else {
-                countDowning()
+                clearInterval(timer)
             }
         }, 1000);
     }
 
     // 向服务器请求发送验证码
     const sendVerificationCode = () => {
-        countDown()
+        // 在localStorage中储存下一次可发验证码时间
+        localStorage.setItem('sxzx-ht-sendVC-lockEnd', new Date().getTime() + maxLockSecond * 1000)
+        // 开始倒计时
+        setLockTimer(maxLockSecond)
         api.RequestVC({
             account: props.account
         }).then((response)=>{
             message.success(response.data.msg)
         }
-        ).catch((error) => {
+        ).catch(() => {
             message.error("获取验证码错误")
         })
     }
@@ -71,10 +72,10 @@ export default function CountDownButton(props) {
         <Button
             block={true}
             onClick={sendVerificationCode}
-            disabled={countDownStatus.isGetting}
-            loading={countDownStatus.isGetting}
+            disabled={buttonLock}
+            loading={buttonLock}
         >
-            {countDownStatus.text}
+            {buttonText}
         </Button>
     )
 }
